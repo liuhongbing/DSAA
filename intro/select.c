3ebe94d87b332155d0fb66339208823944fed7f1
liuhongbing <liuhongbing10000@gmail.com>
2014-11-08 13:16:05 +0800

    把冒泡排序和返回第k大的数改写成函数

--- intro/select.c
+++ intro/select.c
@@ -2,33 +2,51 @@
 
 #define ARRAYSIZE 100
 
+void bublesort(int *parray, int arraysize)
+{
+    int i, j;
+    int temp;
+    
+    if(parray == NULL)
+        return;
+
+    for(i = 0; i < arraysize; i++)
+    {
+        for(j = 0; j < arraysize - i - 1; j++)
+        {
+            if(parray[j + 1] > parray[j])
+            {
+                temp = parray[j + 1];
+                parray[j + 1] = parray[j];
+                parray[j] = temp; 
+            }
+        }
+    }
+    
+    return;
+}
+
+int findkthnum(int *parray, int arraysize, int k)
+{
+    if(parray == NULL)
+        return -1;
+
+    bublesort(parray, arraysize);
+
+    return parray[k - 1];
+} 
+
 int main(int argc, char *argv[])
 {
-    int n, i, j;
-    int temp;
+    int n;
     int array[ARRAYSIZE];
 
     n = 0;
 
     while(scanf("%d", &array[n]) != EOF)
-    {
         n++;
-    }
 
-    for(i = 0; i < n; i++)
-    {
-        for(j = 0; j < n - i - 1; j++)
-        {
-            if(array[j + 1] > array[j])
-            {
-                temp = array[j + 1];
-                array[j + 1] = array[j];
-                array[j] = temp; 
-            }
-        }
-    }
-
-    printf("The %dth number is %d\n", n/2, array[n/2 - 1]);
+    printf("The %dth number is %d\n", n/2, findkthnum(array, n, n/2));
 
     return 0;
 }
